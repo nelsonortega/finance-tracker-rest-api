d4dc8807bdd7a75767ebe035ebc474c3fcb65431
Nelson <neor9712@gmail.com>
2021-09-08 23:00:49 -0600

    Renamed model folder to models

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,4 @@
-import { User } from '../model/User'
+import { User } from '../models/User'
 import { Request, Response } from 'express'
 import userDatabaseHandler from "../database/databaseHandlers/UserDatabaseHandler"
 
